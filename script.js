0da19975eb983f414594fd0c81a3596844c02bc0
sumitrathore <114568634+devil9931@users.noreply.github.com>
2025-08-19 23:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function isSameType(value1, value2) {
   return typeof value1 === typeof value2;
 }
 }
-console.log(isSameType(1, 3));        
-console.log(isSameType("hey", "hi")); 
-console.log(isSameType(NaN, NaN));    
-console.log(isSameType("3", 3));      
-console.log(isSameType(true, false)); 
+// console.log(isSameType(1, 3));        
+// console.log(isSameType("hey", "hi")); 
+// console.log(isSameType(NaN, NaN));    
+// console.log(isSameType("3", 3));      
+// console.log(isSameType(true, false)); 
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
